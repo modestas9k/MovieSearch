7b7bb13de101749aca22fa173cce9d5492877a69
Modestas Skirius <modestas.skirius@gmail.com>
2021-02-24 19:10:57 +0200

    created wantToWatch page

---
+++ src/views/WantToWatch.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>Want to watch</h1>
+    <div class="card__wrapper" >
+      <Card
+        v-for="result in results"
+        :key="result.id"
+        :title="result.name"
+        :premiered="result.premiered"
+        :image="result.image"
+        :data="result"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from '../components/Card'
+import { mapGetters } from 'vuex'
+export default {
+  name: 'WantToWatch',
+  components: {Card},
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+    ...mapGetters({ results: 'getWantToWatch' })
+  }
+}
+</script>
+
+<style>
+
+</style>
