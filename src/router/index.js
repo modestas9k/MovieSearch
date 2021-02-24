5ecd2b3e2d115f81e13076bc7ba1decff88e45d9
Modestas Skirius <modestas.skirius@gmail.com>
2021-02-24 19:13:31 +0200

    add alreadyWatched, wantToWatch to router

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../views/Home'
 import ShowInfo from '../views/ShowInfo'
+import AlreadyWatched from '../views/AlreadyWatched'
+import WantToWatch from '../views/WantToWatch'
 
 Vue.use(Router)
 
@@ -17,6 +19,16 @@ export default new Router({
       name: 'showInfo',
       component: ShowInfo,
       props: true
+    },
+    {
+      path: '/alreadyWatched',
+      name: 'alreadyWatched',
+      component: AlreadyWatched
+    },
+    {
+      path: '/wantToWatch',
+      name: 'wantToWatch',
+      component: WantToWatch
     }
   ]
 })
