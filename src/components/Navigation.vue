608fb8650f15ae83891b7d59840b679fc71e3624
Modestas Skirius <modestas.skirius@gmail.com>
2021-02-27 13:34:30 +0200

    add some style

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,12 +1,20 @@
 <template>
-  <nav>
-    <span>TV Show</span>
-    <div class="link-wrapper">
-      <router-link to="/">Search</router-link>
-      <router-link to="/alreadyWatched">Already watched</router-link>
-      <router-link to="/wantToWatch">Want to watch</router-link>
-    </div>
-  </nav>
+  <div class="nav-wrapper">
+    <nav>
+      <span>TV Show</span>
+      <div class="link-wrapper">
+        <router-link
+          :class="$route.path == '/' ? 'active' : ''"
+          to="/">Search</router-link>
+        <router-link
+          :class="$route.path == '/alreadyWatched' ? 'active' : ''"
+          to="/alreadyWatched">Already watched</router-link>
+        <router-link
+          :class="$route.path == '/wantToWatch' ? 'active' : ''"
+          to="/wantToWatch">Want to watch</router-link>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <script>
@@ -16,6 +24,10 @@ export default {
 </script>
 
 <style scoped>
+  .nav-wrapper {
+    background-color: #213747;
+    padding: 0 20px;
+  }
   nav {
     display: flex;
     flex-direction: row;
@@ -23,6 +35,7 @@ export default {
     justify-content: space-between;
     max-width: 960px;
     margin: 0 auto;
+
   }
   .link-wrapper {
     display: flex;
@@ -31,11 +44,12 @@ export default {
     text-decoration: none;
     color: #f5f5f5;
     font-weight: 600;
+    margin-top: 7px;
     padding: 10px 20px;
-    border-radius: 0px 0px 10px 10px;
+    border-radius: 10px 10px 0px 0px;
     transition: all 0.3s ease-in-out;
   }
-  .link-wrapper a:hover {
-    background-color: #213747;
+  .active {
+    background-color: #4c708b;
   }
 </style>
